415f14ff210a7979ad4b61d2873bebf75dccb247
mariia993 <104144789+mariia993@users.noreply.github.com>
2022-06-07 20:02:51 +0300

    Add files via upload

---
+++ homework-1.js
@@ -0,0 +1,19 @@
+let x=1;
+let y=2;
+
+let res1=""+x+y;
+console.log(res1); // "12"
+console.log(typeof res1); // "string"
+
+let res2=true+""+2;
+console.log(res2); // "true2"
+console.log(typeof res2); // "string"
+
+
+let res3=true;
+console.log(res3); // true
+console.log(typeof res3); // "boolean"
+
+let res4=NaN;
+console.log(res4); // NaN
+console.log(typeof res4); // "number"
