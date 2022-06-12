b49e1b91d0a9b1c51ab696d6971fbf502a02ab6a
mariia993 <104144789+mariia993@users.noreply.github.com>
2022-06-12 15:36:52 +0300

    Add files via upload

---
+++ homeworks-7-1.js
@@ -0,0 +1,47 @@
+let goodEvening=[17, 23];
+
+let i=1;
+for (; i< goodEvening.length; i++) {
+    if (!goodEvening[i]);
+        console.log("good evening");
+};
+
+let goodMorning=[5, 11];
+
+let j=1;
+for (; j< goodEvening.length; j++) {
+    if (!goodEvening[j]);
+        console.log("good morning!");
+};
+
+let goodDay=[11, 17];
+
+let a=1;
+for (; a< goodEvening.length; a++) {
+    if (!goodEvening[a]);
+        console.log("good day!");
+};
+
+let goodNigth=[23, 5];
+
+let k=1;
+for (; k< goodEvening.length; k++) {
+    if (!goodEvening[k]);
+        console.log("good nigth!");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
